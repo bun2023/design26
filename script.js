e4ea755b74a5ac01d7b3e1820cc453538c6e527c
bun2023 <138096357+bun2023@users.noreply.github.com>
2023-08-06 09:17:03 +0900

    portfolio
    Webデザイン練習でオリジナルサイト「企業コーポレートサイト3」

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+
+  let navbar = document.querySelector('.navbar');
+
+  document.querySelector('#menu').onclick = () => {
+    navbar.classList.toggle('active');
+  }  
+
+  window.onscroll = () => {
+    navbar.classList.remove('active');
+  }
+}
